7141a8f26ec09e77da98bffb97a742def7c9309e
Brian Dang <long.dang@student.unsw.edu.au>
2025-09-04 02:18:54 +1000

    fixed error in supabaseClient

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -4,6 +4,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseKey = process.env.REACT_APP_ANON_KEY
 
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase environment variables');
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
